8979f201bc6055c6f4ebcd52eb5df7d009665554
nestoor22 <nestorslavko45@gmail.com>
2019-09-07 18:03:18 +0300

    Fix error with select element

--- dream_house/models.py
+++ dream_house/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
-# class DataToPredict(models.Model):
-#     pass
+class DataToPredict(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.FloatField(default=None)
+    area = models.FloatField(default=None)
+    rooms = models.IntegerField(default=None)
+    floor = models.IntegerField(default=None)
+
+
+    pass
 
 
 class Profile(models.Model):
