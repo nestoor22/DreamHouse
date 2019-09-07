0d6f6288da4dfd1322806c90abd45c770072b854
nestoor22 <nestorslavko45@gmail.com>
2019-09-07 18:24:56 +0300

    Add data to predict Model

--- dream_house/models.py
+++ dream_house/models.py
@@ -2,16 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class DataToPredict(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.FloatField(default=None)
     area = models.FloatField(default=None)
     rooms = models.IntegerField(default=None)
     floor = models.IntegerField(default=None)
-
-
-    pass
+    building_type = models.CharField(max_length=40, default=None)
+    distance_to_center = models.FloatField(default=None)
+    living_area = models.FloatField(default=None)
+    kitchen_area = models.FloatField(default=None)
+    condition = models.CharField(max_length=40, default=None)
+    walls_material = models.CharField(max_length=40, default=None)
+    balconies = models.IntegerField(default=None)
+    ceiling_height = models.FloatField(default=None)
+    floors = models.IntegerField(default=None)
 
 
 class Profile(models.Model):
