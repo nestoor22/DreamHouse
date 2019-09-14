96dbcbb8a157033b0dbf0ec2e057244554166dd2
nestoor22 <nestorslavko45@gmail.com>
2019-09-14 14:30:40 +0300

    Add UTC_ENABLE for Celery

--- DreamHouse/settings.py
+++ DreamHouse/settings.py
@@ -155,3 +155,4 @@ REDIS_HOST = 'localhost'
 REDIS_PORT = '6379'
 BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+UTC_ENABLE = True
