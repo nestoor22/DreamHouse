83d0ca60f63f06154579dd389416f3fb2dc01a4c
nestoor22 <nestorslavko45@gmail.com>
2020-01-01 01:14:14 +0200

    Make statistic page. Add js functions for counters. Now counters starts after scrolling to element

--- data_analysis/views.py
+++ data_analysis/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'graphsPage.html', {'onclick': 'return true'})
+    return render(request, 'aboutDataPage.html', {'onclick': 'return true'})
--- staticfiles/data_analysis/static/js/scripts.js
+++ staticfiles/data_analysis/static/js/scripts.js
@@ -47,3 +47,113 @@ $(document).ready(function(){
 $('.tabs').tabs();
 });
 
+$(document).ready(function () {
+    let show = true;
+    const countbox = ".stats_value";
+    $(window).on("scroll load resize", function () {
+        if (!show)  $(this).text($(this).text()+ ' proposals');
+        const w_top = $(window).scrollTop();
+        const e_top = $(countbox).offset().top;
+        const w_height = $(window).height();
+        const d_height = $(document).height();
+        const e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.stats_value').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 5000,
+                    easing: 'swing',
+                    step: function (now) {
+                        var thousands = Math.floor(Math.ceil(now) / 1000);
+                        if (thousands === 0) {
+                            $(this).text(Math.ceil(now) + '  proposals');
+                        } else {
+                            var hundreds = Math.ceil(now) % 1000;
+                            if (!isNaN(thousands)){
+                                $(this).text(thousands + ' ' + hundreds + '  proposals');
+                            }
+                        }
+                    }
+                });
+            });
+        }
+        show = false;
+    });
+});
+
+$(document).ready(function () {
+    let show = true;
+    const countbox = ".price_value";
+    $(window).on("scroll load resize", function () {
+        if (!show)  $(this).text($(this).text()+ ' proposals');
+        const w_top = $(window).scrollTop();
+        const e_top = $(countbox).offset().top;
+        const w_height = $(window).height();
+        const d_height = $(document).height();
+        const e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.price_value').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function (now) {
+                        var thousands = Math.floor(Math.ceil(now) / 1000);
+                        if (thousands === 0) {
+                            $(this).text(Math.ceil(now) + '  $');
+                        } else {
+                            var hundreds = Math.ceil(now) % 1000;
+                            if (!isNaN(thousands)){
+                                if (hundreds===0){
+                                    hundreds = '000';
+                                }
+                                $(this).text(thousands + ' ' + hundreds + '  $');
+                            }
+                        }
+                    }
+                });
+            });
+        }
+        show = false;
+    });
+});
+
+$(document).ready(function () {
+    let show = true;
+    const countbox = ".area_value";
+    $(window).on("scroll load resize", function () {
+        if (!show)  $(this).text($(this).text()+ ' m²');
+        const w_top = $(window).scrollTop();
+        const e_top = $(countbox).offset().top;
+        const w_height = $(window).height();
+        const d_height = $(document).height();
+        const e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.area_value').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 5000,
+                    easing: 'swing',
+                    step: function (now) {
+                        var thousands = Math.floor(Math.ceil(now) / 1000);
+                        if (thousands === 0) {
+                            $(this).text(Math.ceil(now) + '  m²');
+                        } else {
+                            var hundreds = Math.ceil(now) % 1000;
+                            if (!isNaN(thousands)){
+                                if (hundreds===0){
+                                    hundreds = '000';
+                                }
+                                $(this).text(thousands + ' ' + hundreds + '  m²');
+                            }
+                        }
+                    }
+                });
+            });
+        }
+        show = false;
+    });
+});
